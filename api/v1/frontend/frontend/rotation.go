813c0664f60a7c9d0c32616b4e9e563e0e2cae71
AsuraTu <tugecn@gmail.com>
2023-04-06 09:30:17 +0800

    变更

---
+++ api/v1/frontend/frontend/rotation.go
@@ -0,0 +1,20 @@
+package frontend
+
+import (
+	v1 "shop/api/v1"
+
+	"github.com/gogf/gf/v2/frame/g"
+)
+
+type RotationGetListCommonReq struct {
+	g.Meta `path:"/frontend/rotation/list" method:"get" tags:"轮播图" summary:"轮播图列表接口"`
+	Sort   int `json:"sort" in:"query" dc:"排序"`
+	v1.CommonPaginationReq
+}
+
+type RotationGetListCommonRes struct {
+	List  interface{} `json:"list" description:"列表"`
+	Page  int         `json:"page" description:"分页码"`
+	Size  int         `json:"size" description:"分页数量"`
+	Total int         `json:"total" description:"数据总数"`
+}
